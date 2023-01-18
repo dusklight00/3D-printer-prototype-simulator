7aaaf08863994daa8257e702ca957749bf2ee117
Dusk Light <r.rahul.developer@gmail.com>
2023-01-19 02:03:43 +0530

    getIncompleteOrder() function complete in backend-wrapper.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { addOrder, getOrders } from './modules/backend-wrapper.js';
+import {
+  addOrder,
+  getIncompleteOrders,
+  getOrders,
+} from './modules/backend-wrapper.js';
 import completeOrder from './modules/controller.js';
 
 (async function () {
-  // await addOrder('pratul', 4, 0);
-  const result = await getOrders();
-  const order = {
-    home: 1,
-    completionTime: 4,
-  };
-  completeOrder(result[0]);
+  await addOrder('pratul', 4, 0);
+  const result = await getIncompleteOrders();
+  await completeOrder(result[0]);
+  console.log(await getIncompleteOrders());
 })();
