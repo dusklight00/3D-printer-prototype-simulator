d8c46b4d074a6fb0ba09419cdd7427eb2913b8c8
Dusk Light <r.rahul.developer@gmail.com>
2023-01-18 14:00:30 +0530

    delivery man manager working without bugs

--- main.js
+++ main.js
@@ -1,112 +1,77 @@
+import Printer from './modules/printer.js';
 import PIXIWrapper from './wrappers/pixi-wrapper.js';
+import PrinterManager from './modules/printer-manager.js';
 import City from './modules/city.js';
 import Graph from './modules/graph.js';
+import { cityConfig, graphConfig } from './data.js';
 import DeliveryMan from './modules/delivery-man.js';
-import PrinterSprite from './sprites/printer-sprite.js';
-import Printer from './modules/printer.js';
-import PrinterManager from './modules/printer-manager.js';
-
-const cityConfig = {
-  homes: [
-    {
-      x: 50,
-      y: 50,
-      width: 100,
-      height: 100,
-      label: '1',
-    },
-  ],
-  buildings: [
-    // {
-    //   x: 300,
-    //   y: 100,
-    //   width: 100,
-    //   height: 100,
-    // },
-  ],
-  machines: [
-    {
-      index: 0,
-      x: 200,
-      y: 200,
-      width: 100,
-      height: 100,
-      label: '1',
-    },
-  ],
-};
-
-const graphConfig = {
-  machines: [{ x: 200, y: 200, connectingNode: 2 }],
-  homes: [
-    {
-      x: 50,
-      y: 50,
-      connectingNode: 0,
-    },
-  ],
-  nodes: [
-    {
-      index: 0,
-      x: 100,
-      y: 200,
-    },
-    {
-      index: 1,
-      x: 200,
-      y: 100,
-    },
-    {
-      index: 2,
-      x: 300,
-      y: 200,
-    },
-    {
-      index: 3,
-      x: 400,
-      y: 100,
-    },
-    {
-      index: 4,
-      x: 200,
-      y: 300,
-    },
-  ],
-  matrix: [
-    [0, 1, 0, 0, 0],
-    [1, 0, 1, 0, 0],
-    [0, 1, 0, 1, 1],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-  ],
-};
 
 class DeliveryManManager {
-  constructor(app, graph, city, manager, startingNode, numOfDeliveryMan) {
-    this.mans = [];
-    this.loadDeliveryMans(numOfDeliveryMan, startingNode);
-  }
-  loadDeliveryMans(numOfDeliveryMan, startingNode) {
+  constructor(
+    app,
+    graph,
+    city,
+    printerManager,
+    numOfDeliveryMan,
+    startingNode
+  ) {
+    this.men = [];
+    this.queue = [];
     for (let i = 0; i < numOfDeliveryMan; i++) {
       const man = new DeliveryMan(app, graph, city, startingNode);
-      this.mans.push(man);
+      this.men.push({
+        object: man,
+        isBusy: false,
+      });
     }
   }
-}
-
-class OrderManager {
-  constructor(orders, printerManager, deliveryManager) {
-    this.orders = orders;
-    this.printerStatus = [];
-    this.printerManager = printerManager;
-    this.deliveryManager = deliveryManager;
+  addOrder(order) {
+    this.queue.push(order);
   }
-
+  getUnshippedOrders() {
+    const orders = [];
+    this.queue.forEach((order) => {
+      if (order.isShipped) return;
+      if (order.isShipping) return;
+      orders.push(order);
+    });
+    return orders;
+  }
+  getLastUnshippedOrder() {
+    const unshippedOrders = this.getUnshippedOrders();
+    if (unshippedOrders.length == 0) return null;
+    const lastUnshippedOrder = unshippedOrders[0];
+    return lastUnshippedOrder;
+  }
+  getFreeMen() {
+    const men = [];
+    this.men.forEach((man) => {
+      const isBusy = man.isBusy;
+      if (!isBusy) men.push(man);
+    });
+    return men;
+  }
+  getLastFreeMen() {
+    const men = this.getFreeMen();
+    if (men.length === 0) return null;
+    return men[0];
+  }
+  async deliveryLastUnshippedOrder() {
+    const man = this.getLastFreeMen();
+    const order = this.getLastUnshippedOrder();
+    if (man === null) return;
+    if (order === null) return;
+    const homeIndex = order.home;
+    const machineIndex = order.machine;
+    man.isBusy = true;
+    order.isShipping = true;
+    await man.object.completeOrder(homeIndex, machineIndex);
+    man.isBusy = false;
+  }
   start() {
-    const FRAME_RATE = 30;
+    const FRAME_RATE = 1;
     this.animation = setInterval(() => {
-      if (!this.orders.length) return;
-      const order = this.orders[0];
+      this.deliveryLastUnshippedOrder();
     }, 1000 / FRAME_RATE);
   }
 }
@@ -114,32 +79,42 @@ class OrderManager {
 const container = document.querySelector('.city-container');
 const app = new PIXIWrapper(container);
 
-const graph = new Graph(graphConfig);
-graph.render(app);
-
+const graph = new Graph(app, graphConfig, true);
 const city = new City(app, cityConfig);
-const manager = new PrinterManager('printer1', 'printer2', 'printer3');
-manager.start();
-manager.addQueue(4);
-manager.addQueue(4);
-manager.addQueue(4);
-manager.addQueue(4);
-manager.addQueue(4);
-manager.addQueue(4);
 
-const man = new DeliveryMan(app, graph, city, 0);
+const printerManager = new PrinterManager(
+  city,
+  'printer1',
+  'printer2',
+  'printer3'
+);
+printerManager.start();
 
-const order = {
-  machine: 0,
+const deliveryManager = new DeliveryManManager(
+  app,
+  graph,
+  city,
+  printerManager,
+  2,
+  0
+);
+deliveryManager.start();
+
+const order1 = {
   home: 0,
-  completionTime: 5,
-  printerAllocated: 0,
+  machine: 0,
+  completionTime: 10,
+  isShipped: false,
+  isShipping: false,
 };
 
-// man.completeOrder(order);
+const order2 = {
+  home: 0,
+  machine: 1,
+  completionTime: 10,
+  isShipped: false,
+  isShipping: false,
+};
 
-// man.moveShortestPath(3);
-
-// const graph = new Graph(graphConfig);
-// const result = graph.getHomeConnectingNode(0);
-// console.log(result);
+deliveryManager.addOrder(order1);
+deliveryManager.addOrder(order2);
